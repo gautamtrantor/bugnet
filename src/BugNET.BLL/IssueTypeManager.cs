fec319b8e03abb9dc69db36542b9143199fea740
wrhighfield <wrhighfield@localhost>
2011-11-20 11:19:25 -0600

    Fixed an issue where the re-factored code was not updating an existing saved query properly Updated the SqlDataProvider.cs file to make sure that all .NET objects were being disposed properly by implementing the 'using' construct - removed some redundant code - refactored some method names [only private methods not public] Added a new feature to Milestones to check if the milestone can be deleted safely, if the milestone is assiged to an issue (Affected Milestone and Milestone) it cannot be deleted - added a new stored procedure [BugNet_ProjectMilestones_CanDeleteMilestone] to the Latest.SqlDataProvider.sql - implemented new method in SqlDataProvider.cs [CanDeleteMilestone] - implemented new check logic in the MilestoneManager.cs [Delete] method to use new data method - implemented a returnable string to indicate to the user why the milestone cannot be deleted - implemented the BugNET.Message on the milestone user control for indication - added generic resource exception string [DeleteItemAssignedToIssueError] for the message to user

---
+++ src/BugNET.BLL/IssueTypeManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using BugNET.Common;
+using BugNET.DAL;
+using BugNET.Entities;
+using log4net;
+
+namespace BugNET.BLL
+{
+    public static class IssueTypeManager
+    {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Saves this instance.
+        /// </summary>
+        /// <returns></returns>
+        public static bool SaveOrUpdate(IssueType entity)
+        {
+            if (entity == null) throw new ArgumentNullException("entity");
+            if (entity.ProjectId <= Globals.NEW_ID) throw (new ArgumentException("Cannot save issue type, the project id is invalid"));
+            if (string.IsNullOrEmpty(entity.Name)) throw (new ArgumentException("The issue type name cannot be empty or null"));
+
+            if (entity.Id > Globals.NEW_ID)
+                return DataProviderManager.Provider.UpdateIssueType(entity);
+
+            var tempId = DataProviderManager.Provider.CreateNewIssueType(entity);
+
+            if (tempId <= 0)
+                return false;
+
+            entity.Id = tempId;
+            return true;
+        }
+
+
+        /// <summary>
+        /// Gets the IssueType by id.
+        /// </summary>
+        /// <param name="issueTypeId">The IssueType id.</param>
+        /// <returns></returns>
+        public static IssueType GetById(int issueTypeId)
+        {
+            if (issueTypeId <= Globals.NEW_ID) throw (new ArgumentOutOfRangeException("issueTypeId"));
+
+            return DataProviderManager.Provider.GetIssueTypeById(issueTypeId);
+        }
+
+        /// <summary>
+        /// Deletes the type of the issue.
+        /// </summary>
+        /// <param name="issueTypeId">The issue type id.</param>
+        /// <returns></returns>
+        public static bool DeleteIssueType(int issueTypeId)
+        {
+            if (issueTypeId <= Globals.NEW_ID) throw (new ArgumentOutOfRangeException("issueTypeId"));
+
+            return DataProviderManager.Provider.DeleteIssueType(issueTypeId);
+        }
+
+        /// <summary>
+        /// Gets the issue type by project id.
+        /// </summary>
+        /// <param name="projectId">The project id.</param>
+        /// <returns></returns>
+        public static List<IssueType> GetByProjectId(int projectId)
+        {
+            if (projectId <= Globals.NEW_ID) throw (new ArgumentOutOfRangeException("projectId"));
+
+            return DataProviderManager.Provider.GetIssueTypesByProjectId(projectId);
+        }
+
+        /// <summary>
+        /// Stewart Moss
+        /// Apr 14 2010 
+        /// 
+        /// Performs a query containing any number of query clauses on a certain projectID
+        /// </summary>
+        /// <param name="projectId"></param>
+        /// <param name="queryClauses"></param>
+        /// <returns></returns>
+        public static List<IssueType> PerformQuery(int projectId, List<QueryClause> queryClauses)
+        {
+            if (projectId < 0) throw new ArgumentOutOfRangeException("projectId", "projectI d must be bigger than 0");
+
+            queryClauses.Add(new QueryClause("AND", "ProjectID", "=", projectId.ToString(), System.Data.SqlDbType.Int, false));
+
+            return PerformQuery(queryClauses);
+        }
+
+        /// <summary>
+        /// Stewart Moss
+        /// Apr 14 2010 8:30 pm
+        /// 
+        /// Performs any query containing any number of query clauses
+        /// WARNING! Will expose the entire IssueType table, regardless of 
+        /// project level privledges. (thats why its private for now)
+        /// </summary>        
+        /// <param name="queryClauses"></param>
+        /// <returns></returns>
+        private static List<IssueType> PerformQuery(List<QueryClause> queryClauses)
+        {
+            if (queryClauses == null) throw new ArgumentNullException("queryClauses");
+
+            var lst = new List<IssueType>();
+            DataProviderManager.Provider.PerformGenericQuery(ref lst, queryClauses, @"SELECT a.*, b.ProjectName as ProjectName from BugNet_ProjectIssueTypes as a, BugNet_Projects as b  WHERE a.ProjectID=b.ProjectID ", @" ORDER BY a.ProjectID, a.IssueTypeID ASC");
+
+            return lst;
+        }
+    }
+}
