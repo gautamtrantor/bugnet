ab4012ccb21e579e85cd9e91780d97e48b6f1f91
wrhighfield <wrhighfield@localhost>
2012-04-05 19:52:34 -0500

    Removed extra closing </p> tag in markup

---
+++ src/BugNET_WAP/Administration/Host/UserControls/NotificationSettings.ascx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+using BugNET.BLL;
+using BugNET.BLL.Notifications;
+using BugNET.Common;
+using BugNET.UserInterfaceLayer;
+
+namespace BugNET.Administration.Host.UserControls
+{
+    public partial class NotificationSettings : System.Web.UI.UserControl, IEditHostSettingControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region IEditHostSettingControl Members
+
+        /// <summary>
+        /// Updates this instance.
+        /// </summary>
+        public bool Update()
+        {
+          
+            var notificationTypes = cblNotificationTypes.Items.Cast<ListItem>().Where(li => li.Selected).Aggregate(string.Empty, (current, li) => current + (li.Value + ";"));
+
+            notificationTypes = notificationTypes.TrimEnd(';');
+                
+            HostSettingManager.UpdateHostSetting(HostSettingNames.EnabledNotificationTypes,notificationTypes);
+            HostSettingManager.UpdateHostSetting(HostSettingNames.AdminNotificationUsername, AdminNotificationUser.SelectedValue);
+            return true;
+        }
+
+        /// <summary>
+        /// Inits this instance.
+        /// </summary>
+        public void Initialize()
+        {
+            cblNotificationTypes.DataSource = NotificationManager.Instance.GetNotificationTypes();
+            cblNotificationTypes.DataTextField = "Name";
+            cblNotificationTypes.DataValueField = "Name";
+            cblNotificationTypes.DataBind();
+
+            var notificationTypes = HostSettingManager.Get(HostSettingNames.EnabledNotificationTypes).Split(';');
+            foreach (var currentCheckBox in notificationTypes.Select(s => cblNotificationTypes.Items.FindByText(s)).Where(currentCheckBox => currentCheckBox != null))
+            {
+                currentCheckBox.Selected = true;
+            }
+
+            var users = UserManager.GetAllUsers();
+
+            AdminNotificationUser.DataSource = users;
+            AdminNotificationUser.DataTextField = "DisplayName";
+            AdminNotificationUser.DataValueField = "UserName";
+            AdminNotificationUser.DataBind();
+
+            var adminNotifyUsername = HostSettingManager.Get(HostSettingNames.AdminNotificationUsername);
+            if(users.Where(u=>u.UserName == adminNotifyUsername).SingleOrDefault() != null)
+                AdminNotificationUser.SelectedValue = adminNotifyUsername;
+        }
+
+        #endregion
+    }
+}
